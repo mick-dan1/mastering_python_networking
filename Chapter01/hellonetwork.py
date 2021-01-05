5e329c1a9c57393a4cc5ad8b9b05a25e26812d80
mick-dan1 <rock333.rn@gmail.com>
2021-01-05 19:23:36 +0200

    Update hellonetwork.py

--- Chapter01/hellonetwork.py
+++ Chapter01/hellonetwork.py
@@ -1,4 +1,5 @@
-''' A package is defined by creating a directory. Then you can
+''' 
+A package is defined by creating a directory. Then you can
 place the module source file under that directory. In order for Python 
 to know it as Python-package, just create a __init__.py file in this directory. 
 '''
@@ -6,5 +7,14 @@ from math_stuff import subtract     # from package import module
 result = subtract.subtract(5, 1)   # to call function from module use dot notation
 print(result)
 print('hello network!')
+
+'''
+you can do it slightly different:
+'''
+from math_stuff.subtract import subtract     # from package.module import function
+result = subtract(34, 1)   # calling function to use
+print(result)
+
+
 
 
