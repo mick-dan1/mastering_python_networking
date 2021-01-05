402116d57a4cebca0563a745593f9063876511c4
mick-dan1 <rock333.rn@gmail.com>
2021-01-05 19:04:59 +0200

    add info about package and module
    you have to put __init__.py into package in order for Python to know it as a Python-package

--- Chapter01/hellonetwork.py
+++ Chapter01/hellonetwork.py
@@ -1,3 +1,10 @@
-#This is a comment
+''' A package is defined by creating a directory. Then you can
+place the module source file under that directory. In order for Python 
+to know it as Python-package, just create a __init__.py file in this directory. 
+'''
+from math_stuff import subtract     # from package import module
+result = subtract.subtract(5, 1)   # to call function from module use dot notation
+print(result)
 print('hello network!')
+
 
