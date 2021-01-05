12250bddad8bd3aa66a7b46d2d8e3f469627ca62
mick-dan1 <rock333.rn@gmail.com>
2021-01-05 22:26:46 +0200

    Update import_package_module_function.py

--- Chapter01/import_package_module_function.py
+++ Chapter01/import_package_module_function.py
@@ -9,7 +9,7 @@ print(result)
 
 
 '''
-you can do it slightly different:
+You can do it slightly different: first import a package.module, then call a function
 '''
 from math_stuff.subtract import subtract     # from package/module import function
 result = subtract(34, 1)   # calling function to use
